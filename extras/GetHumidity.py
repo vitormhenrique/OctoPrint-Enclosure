006b6cef77532cdb89016faf15bdbee7bd14ed7a
Vitor de Miranda Henrique <vitormhenrique@gmail.com>
2016-10-10 13:15:44 -0500

    Add files via upload
    DS18B20 bugfix

--- extras/GetHumidity.py
+++ extras/GetHumidity.py
@@ -1,46 +1,26 @@
 #!/usr/bin/python
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-# Modified by Vitor Henrique
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import sys
 import Adafruit_DHT
 
 
 # Parse command line parameters.
-sensor_args = { '11': Adafruit_DHT.DHT11,
-				'22': Adafruit_DHT.DHT22,
-				'2302': Adafruit_DHT.AM2302 }
+sensor_args = {'11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302}
+
 if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-	sensor = sensor_args[sys.argv[1]]
-	pin = sys.argv[2]
-elif len(sys.argv) == 2 and sys.argv[1] == '1820'
-	print 'Failed'
-	sys.exit(1)
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2]
+elif len(sys.argv) == 2 and sys.argv[1] == '1820':
+    print 'Failed'
+    sys.exit(1)
 else:
-	sys.exit(1)
+    sys.exit(1)
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 if humidity is not None:
-	print '{0:0.1f}'.format(humidity)
+    print '{0:0.1f}'.format(humidity)
 else:
-	print 'Failed'
-	sys.exit(1)
+    print 'Failed'
+    sys.exit(1)
--- extras/GetTemperature.py
+++ extras/GetTemperature.py
@@ -1,25 +1,4 @@
 #!/usr/bin/python
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-# Modified by Vitor Henrique
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import sys
 import Adafruit_DHT
 import os
@@ -28,53 +7,54 @@ import time
 
 
 # Parse command line parameters.
-sensor_args = { '11': Adafruit_DHT.DHT11,
-				'22': Adafruit_DHT.DHT22,
-				'2302': Adafruit_DHT.AM2302 }
+sensor_args = {'11': Adafruit_DHT.DHT11, '22': Adafruit_DHT.DHT22, '2302': Adafruit_DHT.AM2302}
 
 if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-	sensor = sensor_args[sys.argv[1]]
-	pin = sys.argv[2]
-	isDHTSensor  = True
-elif len(sys.argv) == 2 and sys.argv[1] == '1820'
-	os.system('modprobe w1-gpio')
-	os.system('modprobe w1-therm')
-	base_dir = '/sys/bus/w1/devices/'
-	device_folder = glob.glob(base_dir + '28*')[0]
-	device_file = device_folder + '/w1_slave'
-	isDHTSensor = False
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2]
+    isDHTSensor = True
+elif len(sys.argv) == 2 and sys.argv[1] == '1820':
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
+    base_dir = '/sys/bus/w1/devices/'
+    device_folder = glob.glob(base_dir + '28*')[0]
+    device_file = device_folder + '/w1_slave'
+    isDHTSensor = False
 else:
-	sys.exit(1)
+    sys.exit(1)
+
 
 def read_temp_raw():
-	f = open(device_file, 'r')
-	lines = f.readlines()
-	f.close()
-	return lines
- 
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
 def read_temp():
-	lines = read_temp_raw()
-	while lines[0].strip()[-3:] != 'YES':
-		time.sleep(0.2)
-		lines = read_temp_raw()
-	equals_pos = lines[1].find('t=')
-	if equals_pos != -1:
-		temp_string = lines[1][equals_pos+2:]
-		temp_c = float(temp_string) / 1000.0
-		return temp_c
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
 
 def read_dht_temp():
-	hum, temp = Adafruit_DHT.read_retry(sensor, pin)
-	return temp
+    hum, temp = Adafruit_DHT.read_retry(sensor, pin)
+    return temp
 
 if isDHTSensor = True:
-	temperature = read_dht_temp()
-	if temperature is not None:
-		print '{0:0.1f}'.format(temperature)
-	else:
-		print 'Failed'
-		sys.exit(1)
-else
-	temperature = read_temp()
-	print '{0:0.1f}'.format(temperature)
-	sys.exit(1)
+    temperature = read_dht_temp()
+    if temperature is not None:
+        print '{0:0.1f}'.format(temperature)
+    else:
+        print 'Failed'
+        sys.exit(1)
+else:
+    temperature = read_temp()
+    print '{0:0.1f}'.format(temperature)
+    sys.exit(1)
