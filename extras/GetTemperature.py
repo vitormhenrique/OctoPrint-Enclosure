0ec5bfdbab7d46adc19c48cad6cf3cd7119559d7
Vitor de Miranda Henrique <vitormhenrique@gmail.com>
2016-10-11 22:01:21 -0500

    Add files via upload
    BugFix

--- extras/GetTemperature.py
+++ extras/GetTemperature.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 import Adafruit_DHT
 
@@ -9,12 +11,23 @@ if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
     sensor = sensor_args[sys.argv[1]]
     pin = sys.argv[2]
 else:
+    print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
+    print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
     sys.exit(1)
 
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
+# Un-comment the line below to convert the temperature to Fahrenheit.
+# temperature = temperature * 9/5.0 + 32
+
+# Note that sometimes you won't get a reading and
+# the results will be null (because Linux can't
+# guarantee the timing of calls to read the sensor).
+# If this happens try again!
 if temperature is not None:
-    print '{0:0.1f}'.format(temperature)
+    print('{0:0.1f}'.format(temperature))
 else:
-    print 'Failed'
+    print('Failed')
     sys.exit(1)
