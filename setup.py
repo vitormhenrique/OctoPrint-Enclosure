0a0e1a2720bb79013e2e872b54b947e0211e43cb
Vitor de Miranda Henrique <vitormhenrique@gmail.com>
2017-04-15 01:59:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
 plugin_requires = ["RPi.GPIO>=0.6",
-		    "git"]
+		    "https://github.com/adafruit/Adafruit_Python_DHT/archive/master.zip"]
 
 #install_requires = requirements("requirements.txt")
 
