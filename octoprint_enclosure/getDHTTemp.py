324a342df6d8b8818fee08eaa12c129ba1c8e6a5
Vitor de Miranda Henrique <vitormhenrique@gmail.com>
2017-04-15 04:18:29 -0500

    Create getDHTTemp.py

---
+++ octoprint_enclosure/getDHTTemp.py
@@ -0,0 +1,20 @@
+import sys
+import Adafruit_DHT
+
+
+# Parse command line parameters.
+sensor_args = { '11': Adafruit_DHT.DHT11,
+    '22': Adafruit_DHT.DHT22,
+        '2302': Adafruit_DHT.AM2302 }
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2]
+else:
+    sys.exit(1)
+
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+print('{0:0.1f}* | {1:0.1f}%'.format(temperature, humidity))
+
+sys.exit(1)
