3a3849e62b11b9936d59a8c7a309ea46dc984fdb
Vitor de Miranda Henrique <vitormhenrique@gmail.com>
2017-04-15 02:55:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ plugin_requires = ["RPi.GPIO>=0.6",
 
 #additional_setup_parameters = []
 
-additional_setup_parameters = {"dependency_links": ["github.com/adafruit/Adafruit_Python_DHT/archive/master.zip"]}
+additional_setup_parameters = {"dependency_links": ["github.com/adafruit/Adafruit_Python_DHT/archive/master.zip#egg=Adafruit_DHT"]}
 
 #install_requires = requirements("requirements.txt")
 
