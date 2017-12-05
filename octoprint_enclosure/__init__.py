651b49306b502f940772241ffa5929234615683b
brbjr1 <bbehrens@liquidframeworks.com>
2017-12-04 18:12:31 -0600

    t7

--- octoprint_enclosure/__init__.py
+++ octoprint_enclosure/__init__.py
@@ -106,8 +106,8 @@ class EnclosurePlugin(octoprint.plugin.StartupPlugin,
             pin = self.toInt(rpi_output['gpioPin'])
             if rpi_output['outputType']=='regular':
                 val = GPIO.input(pin) if not rpi_output['activeLow'] else (not GPIO.input(pin))
-                if (getOutputStatusresult != '')
-                    getOutputStatusresult = getOutputStatusresult + ', '
+               # if (getOutputStatusresult != '')
+               #     getOutputStatusresult = getOutputStatusresult + ', '
                 getOutputStatusresult = getOutputStatusresult + '"' + str(pin) + '":"' + str(val) + '"'
                 
         return '{' + getOutputStatusresult + '}'
