ea736e190f3fc69937db8fed7e718ad2ff8b67c6
vitormhenrique <vitormhenrique@gmail.com>
2020-10-19 21:42:22 +0100

    gitignore and py comp

--- octoprint_enclosure/__init__.py
+++ octoprint_enclosure/__init__.py
@@ -1880,6 +1880,7 @@ class EnclosurePlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplateP
 
 
 __plugin_name__ = "Enclosure Plugin"
+__plugin_pythoncompat__ = ">=3,<4"
 
 
 def __plugin_load__():
