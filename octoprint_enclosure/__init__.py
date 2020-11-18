5b5ec7fc7541b9ef70e643be2d66889aa358d071
ShagoY <shagoy@gmail.com>
2020-11-18 17:45:34 +0100

    Update __init__.py

--- octoprint_enclosure/__init__.py
+++ octoprint_enclosure/__init__.py
@@ -904,7 +904,7 @@ class EnclosurePlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplateP
             stdout = (Popen(cmd, shell=True, stdout=PIPE).stdout).read()
             if  self._settings.get(["debug_temperature_log"]) is True:
                 self._logger.debug("BME280 result: %s", stdout)
-            temp, hum = stdout.split("|")
+            temp, hum = stdout.split(b"|")
             return (self.to_float(temp.strip()), self.to_float(hum.strip()))
         except Exception as ex:
             self._logger.info(
